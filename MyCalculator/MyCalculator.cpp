880f9251758ba2c3ffeb828dd1873bf310d9d20c
Yangcho <vomitrainbow@naver.com>
2022-05-24 09:53:29 +0900

    주석수정

--- MyCalculator/MyCalculator.cpp
+++ MyCalculator/MyCalculator.cpp
@@ -5,6 +5,9 @@ class MainCalc {
 public:
     //자식 클래스가 상속할 기본 클래스
     //사칙 연산자를 가상함수로 여기서 선언
+    //사칙연산 : 연산자로 구현
+    //피연산자 : int, double 가능
+    //피연산자에 따라 서로 다른 연산하도록 동일연산자 중복작성-연산자 오버로딩
 };
 
 class MyCalc : public MainCalc {
@@ -14,5 +17,5 @@ public:
 };
 
 void main() {
-    
+    MyCalc();
 }
